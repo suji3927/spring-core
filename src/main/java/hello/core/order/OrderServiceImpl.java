34e452d47111b676f423f9a03af244d774ea74b0
suji3927 <kim1989suji@gmail.com>
2023-07-25 14:47:48 +0900

    feat: 정율 할인 정책 반영

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,8 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -10,7 +8,15 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // OCP 위반 : 할인 정책 변경에 따른 문제점 : 구현체를 변경해줘야 한다.
+    // 구현체: FixDiscountPolicy -> RateDiscountPolicy
+    // DIP 위반 : OrderServiceImpl이 DiscountPolicy뿐만 아니라 FixDiscountPolicy와 RateDiscountPolicy에도 의존
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // 문제 해결 방법 : 인터페이스에만 의존하도록 코드 변경 -> 그에 따른 문제점 또 발생: NullPointerException 발생
+    // 최종 해결 방안 : 누군가가 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성 후 주입해주어야 한다.
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
